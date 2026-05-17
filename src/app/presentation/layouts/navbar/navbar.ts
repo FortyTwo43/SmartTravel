79a965e94db08180e2c53125c2798c8d75acfa2d
FortyTwo43 <neycraft244@gmail.com>
2026-05-17 18:19:03 -0500

    navbar ahora redirige al login

--- src/app/presentation/layouts/navbar/navbar.ts
+++ src/app/presentation/layouts/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
