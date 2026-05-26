4d539db9f8203fbfbafcf48e3e34c52f6c6230c8
FortyTwo43 <neycraft244@gmail.com>
2026-05-25 19:21:50 -0500

    prueba 4 deploy en vercel

--- set-env.js
+++ set-env.js
@@ -1,20 +1,17 @@
 const fs = require('fs');
 
-const targetPath = './src/app/environments/environment.prod.ts';
-// Puedes cambiar a './public/config.json' si prefieres generar el json.
+const targetPath = './public/config.json';
 
-const envConfigFile = `export const environment = {
-  production: true,
-  supabaseUrl: '${process.env.SUPABASE_URL || ''}',
-  supabaseKey: '${process.env.SUPABASE_KEY || ''}'
-};
-`;
+const envConfigFile = JSON.stringify({
+  supabaseUrl: process.env.SUPABASE_URL || '',
+  supabaseKey: process.env.SUPABASE_KEY || ''
+}, null, 2);
 
 fs.writeFile(targetPath, envConfigFile, function (err) {
   if (err) {
     console.error(err);
     throw err;
   } else {
-    console.log(`Angular environment.prod.ts generated correctly`);
+    console.log(`Angular config.json generated correctly`);
   }
 });
