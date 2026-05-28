778a6a00888a5d54ff87e530810342399a8e5cec
FortyTwo43 <neycraft244@gmail.com>
2026-05-27 21:23:26 -0500

    Los componentes del dashboard del proveedor ahora se encuentra en components/proveedor para mayor organizacion y escalabilidad

--- src/app/presentation/pages/proveedor/proveedor-dashboard/proveedor-dashboard.ts
+++ src/app/presentation/pages/proveedor/proveedor-dashboard/proveedor-dashboard.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProveedorLayoutComponent } from '../../../layouts/proveedor-layout/proveedor-layout';
-import { ProveedorMetricsComponent } from '../../../components/proveedor-metrics/proveedor-metrics';
-import { ProveedorChartPlaceholderComponent } from '../../../components/proveedor-chart-placeholder/proveedor-chart-placeholder';
-import { ProveedorActivityComponent } from '../../../components/proveedor-activity/proveedor-activity';
-import { ProveedorInsightsComponent } from '../../../components/proveedor-insights/proveedor-insights';
-import { ProveedorServicesComponent } from '../../../components/proveedor-services/proveedor-services';
+import { ProveedorMetricsComponent } from '../../../components/proveedor/proveedor-metrics/proveedor-metrics';
+import { ProveedorChartPlaceholderComponent } from '../../../components/proveedor/proveedor-chart-placeholder/proveedor-chart-placeholder';
+import { ProveedorActivityComponent } from '../../../components/proveedor/proveedor-activity/proveedor-activity';
+import { ProveedorInsightsComponent } from '../../../components/proveedor/proveedor-insights/proveedor-insights';
+import { ProveedorServicesComponent } from '../../../components/proveedor/proveedor-services/proveedor-services';
 
 @Component({
   selector: 'app-proveedor-dashboard',
