1bdc5909d421c3b4a45efd2ea6f1d9d1df4c1822
FortyTwo43 <neycraft244@gmail.com>
2026-06-17 22:42:39 -0500

    video

--- src/app/presentation/components/home/home-hero/home-hero.ts
+++ src/app/presentation/components/home/home-hero/home-hero.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { LucideAngularModule, LUCIDE_ICONS, LucideIconProvider, ArrowRight, Play } from 'lucide-angular';
+import { LucideAngularModule, LUCIDE_ICONS, LucideIconProvider, ArrowRight, Play, FileText, AudioLines } from 'lucide-angular';
 
 @Component({
   selector: 'app-home-hero',
@@ -8,9 +8,28 @@ import { LucideAngularModule, LUCIDE_ICONS, LucideIconProvider, ArrowRight, Play
   providers: [{
     provide: LUCIDE_ICONS,
     multi: true,
-    useValue: new LucideIconProvider({ ArrowRight, Play })
+    useValue: new LucideIconProvider({ ArrowRight, Play, FileText, AudioLines })
   }],
   templateUrl: './home-hero.html',
   styleUrl: './home-hero.css',
 })
-export class HomeHero {}
+export class HomeHero {
+  isTranscriptVisible = signal(false);
+  isAudioDescActive = signal(false);
+  transcriptText = signal('');
+
+  constructor() {
+    fetch('assets/videos/transcripcion.txt')
+      .then(res => res.text())
+      .then(text => this.transcriptText.set(text))
+      .catch(err => console.error('Error loading transcription', err));
+  }
+
+  toggleTranscript() {
+    this.isTranscriptVisible.update(v => !v);
+  }
+
+  toggleAudioDesc() {
+    this.isAudioDescActive.update(v => !v);
+  }
+}
