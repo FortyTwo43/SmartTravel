3e389d45756223d9a28a4fb7e0eafa792eb19b2d
jordanlema <joseaalaba8989@gmail.com>
2026-06-24 23:16:46 -0500

    Cambios y realizacion de la accesibilidad

--- src/app/presentation/components/viajero/explorar-destinos/destination-card/destination-card.component.ts
+++ src/app/presentation/components/viajero/explorar-destinos/destination-card/destination-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, input, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import type { ExploreDestination } from '../../../../../useCase/viajero/explorar-destinos/GetExplorarDestinosUseCase';
 import { LucideAngularModule, LUCIDE_ICONS, LucideIconProvider, Heart } from 'lucide-angular';
@@ -8,7 +8,7 @@ import { LucideAngularModule, LUCIDE_ICONS, LucideIconProvider, Heart } from 'lu
 @Component({
   selector: 'app-explore-destination-card',
   standalone: true,
-  imports: [CommonModule, TranslateModule, LucideAngularModule],
+  imports: [CommonModule, TranslateModule, LucideAngularModule, RouterModule],
   providers: [
     {
       provide: LUCIDE_ICONS,
