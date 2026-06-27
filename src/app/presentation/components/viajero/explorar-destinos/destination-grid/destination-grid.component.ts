50efa1502dbbaac31f433ce3201135eec9abf72d
FortyTwo43 <neycraft244@gmail.com>
2026-06-27 00:10:53 -0500

    Se aplicó html semantico y aria al proyecto

--- src/app/presentation/components/viajero/explorar-destinos/destination-grid/destination-grid.component.ts
+++ src/app/presentation/components/viajero/explorar-destinos/destination-grid/destination-grid.component.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { ExploreDestination } from '../../../../../useCase/viajero/explorar-destinos/GetExplorarDestinosUseCase';
 import { DestinationCardComponent } from '../destination-card/destination-card.component';
 
 @Component({
   selector: 'app-destination-grid',
   standalone: true,
-  imports: [CommonModule, DestinationCardComponent],
+  imports: [CommonModule, DestinationCardComponent, TranslateModule],
   templateUrl: './destination-grid.component.html',
   styleUrl: './destination-grid.component.css'
 })
